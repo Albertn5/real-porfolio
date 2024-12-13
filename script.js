ea669f6de89bfd46e122e9ec9b1b9b5a44cc84a7
Albertn5 <nuneza3@tcnj.edu>
2024-12-13 15:05:39 -0500

    update 5

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-
-import { Grid } from '@splidejs/splide-extension-grid';
-
-
-new Splide( '#splide' ).mount( { Grid } );
-
-new Splide( '#splide' ).mount( window.splide.Extensions );
-
-new Splide( '#splide', {
-    grid: {
-          rows: 3,
-          cols: 3,
-          gap : {
-              row: '1rem',
-              col: '1.5rem',
-          },
+new Splide( '.splide', {
+    grid:{
+        rows: 3,
+        cols: 3,
+        gap: {
+            row: '1rem',
+            col: '1.5rem'
+        },
     },
-  } );
+} ).mount( window.splide.Extensions );
